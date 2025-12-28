49831a9e5895debdfa04bd649fecdfdc51b857bd
Venkatesh <vengets@gmail.com>
2025-12-29 03:29:51 +1100

    M0: undertow server with health endpoint and routing

---
+++ src/main/java/com/vk/ratelimiter/Main.java
@@ -0,0 +1,49 @@
+package com.vk.ratelimiter;
+
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.server.handlers.PathHandler;
+import io.undertow.util.Headers;
+import io.undertow.util.StatusCodes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(final String[] args) {
+        int cores = Runtime.getRuntime().availableProcessors();
+        logger.info("Available processors: {}", cores);
+
+        PathHandler pathHandler = new PathHandler();
+        pathHandler.addExactPath("/health", healthHandler());
+        pathHandler.add.addPrefixPath("/", notFoundHandler());
+
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "0.0.0.0")
+                .setHandler(pathHandler).build();
+        server.start();
+    }
+
+    private static HttpHandler notFoundHandler() {
+        return new HttpHandler() {
+
+            @Override
+            public void handleRequest(HttpServerExchange exchange) throws Exception {
+                exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
+                exchange.setStatusCode(StatusCodes.NOT_FOUND);
+                exchange.getResponseSender().send("{\"status\":\"Not Found\"}");
+            }
+        };
+    }
+
+    private static HttpHandler healthHandler() {
+        return new HttpHandler() {
+            @Override
+            public void handleRequest(HttpServerExchange exchange) throws Exception {
+                exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
+                exchange.getResponseSender().send("{\"status\":\"ok\"}");
+            }
+        };
+    }
+}
